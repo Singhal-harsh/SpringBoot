0aea594eaf0db19b1a58f92de2fc87d13a009146
amyglan <f2016197@pilani.bits-pilani.ac.in>
2020-09-20 21:13:21 +0530

    Adding to Project

---
+++ src/main/java/com/citi/springboot/arbproject/ArbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.citi.springboot.arbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citi/springboot/arbproject/ArbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.springboot.arbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
